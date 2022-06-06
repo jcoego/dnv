478db74e357cd09ee08596c52c1321045e0d25e6
jcoegom@gmail.com <jcoegom@gmail.com>
2022-06-06 20:45:36 +0200

    WIP

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = 8000
 const data = require('./data/data.json');
 const _ = require('lodash');
 const cors = require('cors');
+const COMMON_FIELDS = ['*path','*type','*name','*size','*url']
 
 app.use(cors());
 
@@ -15,10 +16,24 @@ app.get('/', (req, res) => {
     path = path.replace(/[/]/g,'.');
 
     let dataRes = path ?  _.get(data, path) : data;
-    console.log('dataRes',dataRes);
+    let result={};
+    for(let key in dataRes){
+      if(key.indexOf('*')!==-1){
+        result[key]=dataRes[key]
+      }else{
+        result[key]={}
+        for(let commonFieldsItem of COMMON_FIELDS){
+          result[key][commonFieldsItem] = dataRes[key][commonFieldsItem] ?  dataRes[key][commonFieldsItem] : ''
+        }
+
+      }
+    }
+
+    console.log('dataRes',dataRes, result);
     //let dataRes =  _.get(data,'myfiles.procesos de logistica');
-    return res.status(200).json(dataRes);
+    return res.status(200).json(result);
   }catch(err){
+    console.log(err)
     //TODO: function to proccess error in a centralized way (send them to log...etc.)
     res.status(500).json({message: 'Unexpected error'})
   }
